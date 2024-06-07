4d7e63ef7c06a7e958ebcb0fe33b5c29f9d2e4f8
Om <031230om@gmail.com>
2024-06-07 16:11:04 -0500

    added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Bhagavad Chatbot',
+    version= '0.0.0',
+    author= 'Om Agrawal',
+    author_email= 'omagrawal@utexas.edu',
+    packages= find_packages(),
+    install_requires = []
+
+)
